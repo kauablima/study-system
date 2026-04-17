fcf1d31a82b8c4b75c1f27e3a07cd3124eea3058
Kauã Lima <dev.kaualima@gmail.com>
2026-04-17 15:20:10 -0300

    feat(back/studySession): add controller

---
+++ backend/src/controller/studySession.ts
@@ -0,0 +1,31 @@
+import type { RequestHandler } from "express";
+import { sessionSchema } from "../schemas/create-sessionr.js";
+import { createStudySession } from "../service/studySession.js";
+
+export const registeStudySession: RequestHandler = async (req, res) => {
+    const result = sessionSchema.safeParse(req.body);
+
+    if(!result.success){
+        res.status(401).json({ error: 'Fill in the field.' })
+        return;
+    }
+
+    //modify this after
+    const data = { 
+        topic: result.data.topic, 
+        duration_minutes: result.data.duration_minutes, 
+        study_date: result.data.study_date, 
+        user_id: result.data.user_id, 
+        subject_id: result.data.subject_id,
+    };
+
+    const session = await createStudySession(data);
+
+    if(!session){
+        res.status(401).json({error: 'Faild to create session'}) 
+        return;   
+    }
+
+
+    return res.status(201).json({message: 'create study session: ', session });
+}
--- backend/src/service/studySession.ts
+++ backend/src/service/studySession.ts
@@ -2,7 +2,7 @@ import { prisma } from "../libs/db.js"
 
 type studySessionDTO = {
     topic: string,
-    durantion_minutes: number,
+    duration_minutes: number,
     study_date: Date,
     user_id: string,
     subject_id: string,
@@ -14,19 +14,19 @@ export const createStudySession = (data: studySessionDTO) => {
             id: data.subject_id,
             user_id: data.user_id,
          }
-    }) 
+    });
 
     if(!existignSubejct) return null;
 
     const studySession = prisma.study_session.create({
         topic: data.topic,
-        durantion_minutes: data.durantion_minutes,
+        durantion_minutes: data.duration_minutes,
         study_date: data.study_date,
         user: { connect: {id: data.user_id} },
         subject: { connect: {id: data.subject_id}}
 
-    })
+    });
 
-    return studySession
+    return studySession;
 }
 
