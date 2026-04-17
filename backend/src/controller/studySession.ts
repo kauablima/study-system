c025c459b67ad784a86b173288ebf4ef9677f25b
Kauã Lima <dev.kaualima@gmail.com>
2026-04-17 16:03:01 -0300

    fix(back/studySession): fix schema import path in controller

--- backend/src/controller/studySession.ts
+++ backend/src/controller/studySession.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "express";
-import { sessionSchema } from "../schemas/create-sessionr.js";
 import { createStudySession } from "../service/studySession.js";
+import { sessionSchema } from "../schemas/create-session.js";
 
 export const registeStudySession: RequestHandler = async (req, res) => {
     const result = sessionSchema.safeParse(req.body);
@@ -9,12 +9,12 @@ export const registeStudySession: RequestHandler = async (req, res) => {
         res.status(401).json({ error: 'Fill in the field.' })
         return;
     }
-
+    
     //modify this after
     const data = { 
         topic: result.data.topic, 
         duration_minutes: result.data.duration_minutes, 
-        study_date: result.data.study_date, 
+        study_date: result.data.study_date , 
         user_id: result.data.user_id, 
         subject_id: result.data.subject_id,
     };
