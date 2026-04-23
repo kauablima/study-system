9c176ed1e0ac5e937ac114b665822234bee20b22
Kauã Lima <dev.kaualima@gmail.com>
2026-04-22 22:24:51 -0300

    test: login route

--- backend/src/router/main.ts
+++ backend/src/router/main.ts
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/auth.js";
+import { registerStudySession } from "../controller/studySession.js";
+import { login } from "../controller/user.js";
 
 const router = Router();
+
+router.post('/login', authMiddleware, login);
+
+//poder testar
+router.post('/studySession', authMiddleware, registerStudySession);
+router.get('/listSessions', authMiddleware,)
+
+export default router;
